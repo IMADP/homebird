569faf1b812eb96ac8bdd314400221c0da8afe51
Anthony DePalma <antdepalma@gmail.com>
2022-12-04 17:01:07 -0500

    validation errors

---
+++ homebird-web/src/components/validation/ValidationErrors.tsx
@@ -0,0 +1,60 @@
+import {
+    Alert,
+    AlertDescription,
+    AlertIcon,
+    AlertTitle, Box, Container, ListItem, UnorderedList
+} from '@chakra-ui/react';
+
+export interface ValidationError {
+    error: string;
+    path: Array<string>;
+    attributes: Map<string, any>;
+}
+
+export interface ValidationErrorProps {
+    errors: Array<ValidationError>;
+}
+
+const getKey = function (error: ValidationError): string {
+    return error.path.join(".") + "." + error;
+};
+
+const getMessage = function (error: ValidationError): string {
+
+    if(error.error === 'inUse') {
+        return `The ${error.path[0]} is currently in use`;
+    }
+
+    return error.error;
+};
+
+/**
+ * RegisterPage
+ * 
+ * @returns 
+ */
+export const ValidationErrors = (props: ValidationErrorProps) => {
+    return (
+        <>
+            {props.errors.length > 0 &&
+                <Box mb="5">
+                    <Alert status='error'>
+                        <AlertIcon />
+                        <AlertTitle>Please fix the following errors</AlertTitle>
+                        <AlertDescription></AlertDescription>
+                    </Alert>
+                    <Alert status='warning'>
+                        <AlertDescription>
+                            <UnorderedList>
+                                {props.errors.map((e) =>
+                                    <ListItem key={getKey(e)} ml="5">{getMessage(e)}</ListItem>
+                                )}
+                            </UnorderedList>
+                        </AlertDescription>
+                    </Alert>
+                </Box>
+            }
+        </>
+    );
+
+}
--- homebird-web/src/pages/register/RegisterPage.tsx
+++ homebird-web/src/pages/register/RegisterPage.tsx
@@ -7,72 +7,153 @@ import {
     FormLabel,
     Heading,
     HStack,
-    Input,
-    Stack,
+    Input, Stack,
     Text, useBreakpointValue, useColorModeValue
-} from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
-import { Logo } from 'components/Logo'
+} from '@chakra-ui/react';
+import { UserApi, UserRequest } from 'api/user-api';
+import { Logo } from 'components/Logo';
+import { ValidationError, ValidationErrors } from 'components/validation/ValidationErrors';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-export const RegisterPage = () => (
-    <Container maxW="md" py={{ base: '12', md: '24' }}>
-        <Stack spacing="8">
-            <Stack spacing="6" align="center">
-                <Logo />
-                <Stack spacing="3" textAlign="center">
-                    <Heading size={useBreakpointValue({ base: 'xs', md: 'sm' })}>Create an account</Heading>
-                    <Text color="muted">Every bird needs a home</Text>
-                </Stack>
-            </Stack>
-            <Box
-                py={{
-                    base: '0',
-                    sm: '8',
-                }}
-                px={{
-                    base: '4',
-                    sm: '10',
-                }}
-                bg={useBreakpointValue({
-                    base: 'transparent',
-                    sm: 'bg-surface',
-                })}
-                boxShadow={{
-                    base: 'none',
-                    sm: useColorModeValue('md', 'md-dark'),
-                }}
-                borderRadius={{
-                    base: 'none',
-                    sm: 'xl',
-                }}
-            >
-                <Stack spacing="6">
-                    <Stack spacing="5">
-                        <FormControl isRequired>
-                            <FormLabel htmlFor="email">Email</FormLabel>
-                            <Input id="email" type="email" />
-                        </FormControl>
-                        <FormControl isRequired>
-                            <FormLabel htmlFor="password">Password</FormLabel>
-                            <Input id="password" type="password" />
-                            <FormHelperText color="muted">At least 8 characters long</FormHelperText>
-                        </FormControl>
+
+/**
+ * RegisterPage
+ * 
+ * @returns 
+ */
+export const RegisterPage = () => {
+
+    // form object
+    const userRequestDefault: UserRequest = {
+        email: "",
+        password: ""
+    };
+
+    const [userRequest, setUserRequest] = useState(userRequestDefault);
+    const [errors, setErrors] = useState<Array<ValidationError>>([]);
+
+    const formHandler = async (ev: React.ChangeEvent<HTMLFormElement>) => {
+        ev.preventDefault();
+        setUserRequest(userRequest);
+
+        await UserApi.createUser(userRequest)
+            .then((response) => {
+                // handle success
+                console.log('success');
+                console.log(response);
+                setUserRequest(userRequestDefault);
+            })
+            .catch((error) => {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2x
+                    setErrors(error.response.data);
+                    return;
+
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                }
+
+            });
+
+
+
+    };
+
+    const handleInputChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+
+        setUserRequest({
+            ...userRequest,
+            [ev.target.name]: ev.target.value
+        })
+    }
+
+    return (
+        <Container
+            maxW="lg"
+            py={{
+                base: '12',
+                md: '24',
+            }}
+            px={{
+                base: '0',
+                sm: '8',
+            }}
+        >
+
+
+            <form onSubmit={formHandler}>
+                <Stack spacing="8">
+                    <Stack spacing="6" align="center">
+                        <Logo />
+                        <Stack spacing="3" textAlign="center">
+                            <Heading size={useBreakpointValue({ base: 'xs', md: 'sm' })}>Create an account</Heading>
+                            <Text color="muted">Every bird needs a home</Text>
+                        </Stack>
                     </Stack>
-                    <Stack spacing="4">
-                        <Button variant="primary">Create account</Button>
-                    </Stack>
+
+                    <Box
+                        py={{
+                            base: '0',
+                            sm: '8',
+                        }}
+                        px={{
+                            base: '4',
+                            sm: '10',
+                        }}
+                        bg={useBreakpointValue({
+                            base: 'transparent',
+                            sm: 'bg-surface',
+                        })}
+                        boxShadow={{
+                            base: 'none',
+                            sm: useColorModeValue('md', 'md-dark'),
+                        }}
+                        borderRadius={{
+                            base: 'none',
+                            sm: 'xl',
+                        }}
+                    >
+
+                        <ValidationErrors errors={errors} />
+
+                        <Stack spacing="6">
+                            <Stack spacing="5">
+                                <FormControl isRequired>
+                                    <FormLabel htmlFor="email">Email</FormLabel>
+                                    <Input id="email" name='email' type="email" value={userRequest.email} onChange={handleInputChange} />
+                                </FormControl>
+                                <FormControl isRequired>
+                                    <FormLabel htmlFor="password">Password</FormLabel>
+                                    <Input id="password" name='password' type="password" value={userRequest.password} onChange={handleInputChange} />
+                                    <FormHelperText color="muted">Please use a password manager</FormHelperText>
+                                </FormControl>
+                            </Stack>
+                            <Stack spacing="4">
+                                <Button variant="primary" type="submit">Create account</Button>
+                            </Stack>
+                        </Stack>
+                    </Box>
+                    <HStack justify="center" spacing="1">
+                        <Text fontSize="sm" color="muted">
+                            Already have an account?
+                        </Text>
+                        <Link to="/login">
+                            <Button variant="link" colorScheme="blue" size="sm">
+                                Log in
+                            </Button>
+                        </Link>
+                    </HStack>
                 </Stack>
-            </Box>
-            <HStack justify="center" spacing="1">
-                <Text fontSize="sm" color="muted">
-                    Already have an account?
-                </Text>
-                <Link to="/login">
-                    <Button variant="link" colorScheme="blue" size="sm">
-                        Log in
-                    </Button>
-                </Link>
-            </HStack>
-        </Stack>
-    </Container>
-)
+
+            </form>
+        </Container>
+    )
+}
